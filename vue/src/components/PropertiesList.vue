62fbd3f5d590fc613c0f7e5978daa5db97bb8c42
Dylan Winters <awsomemon123@gmail.com>
2021-12-07 11:41:54 -0500

    trying to add properties to home vue

---
+++ vue/src/components/PropertiesList.vue
@@ -0,0 +1,27 @@
+<template>
+    <li
+    class="list-group-item  justify-content-between align-items-center"
+  >
+    {{ properties.text }}
+    <span
+      v-bind:class="{
+          id
+      }"
+    >
+      
+    </span>
+  </li>
+</template>
+
+<script>
+
+export default {
+    props: {
+        properties: Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,11 +2,34 @@
   <div class="home">
     <h1>Home</h1>
     <p>Your content goes here. Make it look nice.</p>
+    <ul class="list-group">
+      <properties-list
+      v-for="properties of properties"
+      v-bind:key="properties.id"
+      v-bind:properties="properties"/>
+    </ul>
   </div>
 </template>
 
 <script>
+import PropertiesList from '../components/PropertiesList.vue';
+import PropertiesService from '../services/PropertyService.js';
 export default {
-  name: "home"
+  components: {
+    PropertiesList
+  },
+  name: "home",
+  
+    created() {
+      PropertiesService
+      .getAllProperties()
+      .then(response => {
+        this.PropertiesList = response.data;
+      })
+      .catch(response => {
+        console.error("could not get properties", response);
+      });
+    }
+  
 };
 </script>
