e9506c4c5bb353976625b759f3e44abbb2ffd07d
Chae Roy <chaedoesntcode@gmail.com>
2021-12-09 15:27:00 -0500

    Add splash page changes

--- vue/src/components/PropertiesList.vue
+++ vue/src/components/PropertiesList.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <li
+    <!-- <li
     class="list-group-item  justify-content-between align-items-center" v-if="properties.isAvailable == true">
   <div class="property-name">
     {{ properties.name }}
@@ -21,7 +21,22 @@
     >
       
     </span>
-  </li>
+  </li> -->
+  
+  <div class="card text-left" v-if="properties.isAvailable == true">
+  
+  <img src="../../images/image-placeholder.jpg" class="card-img-top">
+  <div class="card-body">
+    <p class="card-text">
+      {{ properties.name }}</p>
+    <p class="card-text"> 
+      {{properties.street}}
+    {{properties.city}}
+    {{properties.zipcode}}
+    </p>
+  </div>
+</div>
+
 </template>
 
 <script>
@@ -34,7 +49,15 @@ export default {
 </script>
 
 <style>
-  div {
-    text-align: center;
-  }
+ .card {
+  border: none;
+  border-radius: 0;
+  border-bottom: 3px solid #457B9D;
+  margin-bottom: 2rem;
+  width: 15rem;
+  padding: 0;
+}
+.card-body{
+  padding: 1rem 0 1rem 0;
+}
 </style>
