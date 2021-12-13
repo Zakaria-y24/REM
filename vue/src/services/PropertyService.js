5a968acf1e523368d8464a8196abc02f17c2d627
Josh Long <joslong23@gmail.com>
2021-12-13 15:50:38 -0500

    pulling changes

--- vue/src/services/PropertyService.js
+++ vue/src/services/PropertyService.js
@@ -11,7 +11,7 @@ export default {
     addProperty(property) {
          return axios.post('/properties', property);
     },
-    searchProperties(zipcode,beds,baths){
-        return axios.post('/search',zipcode, beds,baths);
+    searchProperties(){
+        return axios.get('/search');
     }
 }
